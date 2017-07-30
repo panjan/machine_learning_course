a80087ed00c699505b758a7f5d518a9159b3fdc8
panjan <jan.zdrahal@gooddata.com>
2017-07-31 00:26:44 +0200

    Add regularized logistic regression gradient

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 
 h = sigmoid(X*theta)
 J = (1/m)*sum(-y'*log(h)-(1-y)'*log(1-h)) + (lambda/(2*m))*sum(theta(2:end).^2)
-grad = (1/m)*X'*(h-y)
+grad = (1/m)*X'*(h-y)+[0;(lambda/m)*theta(2:end)]
 
 % =============================================================
 
